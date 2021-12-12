ab0b79fe53eb61ffd79adf349a286679644f78c3
patrycja <patrycja.komoszynska.gmail.com>
2021-12-12 10:19:20 +0100

    zajęcia - zadania 12.12

--- choinka-torjkaty.py
+++ choinka-torjkaty.py
@@ -4,24 +4,24 @@ wn = turtle.Screen()
 tofik = turtle.Turtle()
 
 
-def rysujtrojkaty(x, y, size, bgcolor):
+def rysujtrojkaty(x, y, rozmair, bgcolor):
     tofik.penup()
     tofik.setposition(x, y)
     tofik.fillcolor(bgcolor)
     tofik.begin_fill()
     tofik.pendown()
-    tofik.goto(x + size, y)
-    tofik.goto(x, y + size)
-    tofik.goto(x - size, y)
+    tofik.goto(x + rozmair, y)
+    tofik.goto(x, y + rozmair)
+    tofik.goto(x - rozmair, y)
     tofik.goto(x, y)
     tofik.end_fill()
 
 
-def rysujchoinke(x, y, size, step, count):
-    for part in range(count):
-        rysujtrojkaty(x, y, size, "green")
-        y = y - 2*step
-        size = size + step
+def rysujchoinke(x, y, rozmiar, krok, ilosc):
+    for part in range(ilosc):
+        rysujtrojkaty(x, y, rozmiar, "green")
+        y = y - 2*krok
+        rozmiar = rozmiar + krok
 
 
 rysujchoinke(0, 0, 40, 20, 8)
